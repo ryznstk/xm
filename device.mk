edb0a5b10a81d84e91ab6208b1480c5e4b6ff73d
Mathieu Chartier <mathieuc@google.com>
2021-11-02 21:34:43 +0530

    peridot: Speed profile services and wifi-service to reduce RAM and storage
    Signed-off-by: Shikhar <Krtonia@gmail.com>

--- device.mk
+++ device.mk
@@ -430,6 +430,9 @@ PRODUCT_PACKAGES += \
     vendor.qti.qspa-service \
     qspa_vendor.rc
 
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # SKU
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/props/odm_CN.prop:$(TARGET_COPY_OUT_ODM)/etc/odm_CN.prop \
