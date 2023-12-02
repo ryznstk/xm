d0648657a48672473847b6df4b80bdff544fd605
SGCMarkus <markusornik@gmail.com>
2023-12-02 23:07:07 +0700

    peridot: Set product characteristics to nosdcard
    Change-Id: Ief0e9d2ced9ebc7fc90300be697dd98c81ea5a48
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: Guidix <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -131,6 +131,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.context_hub.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/android.hardware.context_hub.xml
 
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
