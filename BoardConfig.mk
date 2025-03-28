1bd6691d4b4f3a194045cf03a23dc8b2ed28ee25
kenway214 <kenway214@outlook.com>
2025-03-28 18:02:53 +0530

    peridot: rootdir: Disable serial console
    Signed-off-by: kenway214 <kenway214@outlook.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -119,6 +119,7 @@ BOARD_KERNEL_CMDLINE := \
     mtdoops.fingerprint=peridot:$(LINEAGE_VERSION)
 
 BOARD_BOOTCONFIG := \
+    androidboot.console=0 \
     androidboot.hardware=qcom \
     androidboot.memcg=1 \
     androidboot.usbcontroller=a600000.dwc3 \
