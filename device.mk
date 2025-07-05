689287f5b2fc948e78837761aadec3b2bf9ad395
EmanuelCN <emanuelghub@gmail.com>
2025-07-05 19:20:04 +0000

    peridot: Properly enable UFFD GC
    Signed-off-by: Guidix <guidixm@gmail.com>

--- device.mk
+++ device.mk
@@ -26,6 +26,9 @@ $(call inherit-product, hardware/qcom-caf/common/common.mk)
 # Call the MiuiCamera setup
 $(call inherit-product-if-exists, device/xiaomi/peridot-miuicamera/device.mk)
 
+# ART
+PRODUCT_ENABLE_UFFD_GC := true
+
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
