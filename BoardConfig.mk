c275438e56332da35097980d79ad2e71f7d54f42
New Author Name <120115258+swiitchOFF@users.noreply.github.com>
2025-07-27 19:03:38 +0530

    peridot: Add sepolicy for diag-router and guard it for user build only
    * Needed only for user build
    
    Change-Id: I349daba9e446d4b3d94ab0511ba04328d9257942
    Signed-off-by: swiitchOFF <120115258+swiitchOFF@users.noreply.github.com>
    Signed-off-by: Frost <frost.github@proton.me>
    Signed-off-by: iHSAN <ihsanulrahman@gmail.com>
    Signed-off-by: GuidixX <guidixm@gmail.com>
    Co-Authored-By: Tiernan Hubble <thubble@thubble.ca>
    Co-Authored-By: TogoFire <togofire@mailfence.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -229,6 +229,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 # Sepolicy
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+include $(DEVICE_PATH)/sepolicy/SEPolicy-diag.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
