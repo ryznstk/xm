8530cd1acafd796893a2f8d315ba1b15dc4ed1b9
ryznstk <agusmhrd1k4@gmail.com>
2026-02-08 12:22:14 +0700

    Priv-key

--- device.mk
+++ device.mk
@@ -292,6 +292,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
     frameworks/native/data/etc/android.hardware.keystore.app_attest_key.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.keystore.app_attest_key.xml
 
+# Lineage-Priv
+-include vendor/lineage-priv/keys/keys.mk
+
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
