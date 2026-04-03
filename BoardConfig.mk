2d74acfc93f4c77dcdd8bdd5a7908af3e7cd8309
ryznstk <agsmhrd1k4@gmail.com>
2026-04-03 16:40:19 +0700

    Revert "peridot: Remove parts app sepolicy"
    This reverts commit cd0d868e99fe709a7025615e654b0e682ea353ff.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,6 +219,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Vendor security patch
 VENDOR_SECURITY_PATCH := 2025-12-01
