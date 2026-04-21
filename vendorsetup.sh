6f83f3cab1646e7b8c34c33127c2027df782a164
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-04-21 09:26:46 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -56,10 +56,10 @@ echo "Cloning KProfiles..."
 rm -rf packages/apps/KProfiles
 git clone https://github.com/ryznstk/packages_apps_KProfiles.git packages/apps/KProfiles
 
-cd system/sepolicy
-git fetch https://github.com/ryznstk/system_sepolicy.git bq2
-git reset --hard FETCH_HEAD
-croot
+#cd system/sepolicy
+#git fetch https://github.com/ryznstk/system_sepolicy.git bq2
+#git reset --hard FETCH_HEAD
+#croot
 
 cd device/qcom/sepolicy_vndr/sm8650
 git fetch https://github.com/LineageOS/android_device_qcom_sepolicy_vndr.git lineage-23.2-caf-sm8650
