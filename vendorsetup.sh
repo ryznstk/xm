ded1aa003cb66758e369c696ed5631d864115df8
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-04-27 17:26:08 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -61,16 +61,6 @@ git clone https://github.com/ryznstk/packages_apps_KProfiles.git packages/apps/K
 #git reset --hard FETCH_HEAD
 #croot
 
-cd frameworks/base
-git fetch https://github.com/ryznstk/evo_frameworks_base.git bq2
-git reset --hard FETCH_HEAD
-croot
-
-cd packages/apps/Evolver
-git fetch https://github.com/ryznstk/packages_apps_Evolver.git bq2
-git reset --hard FETCH_HEAD
-croot
-
 cd device/qcom/sepolicy_vndr/sm8650
 git fetch https://github.com/LineageOS/android_device_qcom_sepolicy_vndr.git lineage-23.2-caf-sm8650
 git reset --hard FETCH_HEAD
