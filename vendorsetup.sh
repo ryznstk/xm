dd6f3ef15481dbe31a02bad6b9094bdefe61b366
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-05-03 04:39:51 +0700

    Add AxionFX app repository to vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -44,6 +44,8 @@ rm -rf packages/apps/XiaomiParts
 rm -rf packages/apps/ViPER4AndroidFX
 git clone https://github.com/TogoFire/packages_apps_ViPER4AndroidFX.git packages/apps/ViPER4AndroidFX
 
+git clone https://github.com/AxionAOSP/android_packages_apps_AxionFx.git packages/apps/AxionFX
+
 # LMO
 echo "fetching LMOfreeroam tree..."
 cd packages/apps/LMOFreeform
