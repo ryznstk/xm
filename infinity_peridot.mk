6f61cb5e2274d22ddbd6b066fc8ae58894fe1721
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-05-02 10:33:56 +0700

    Rename to infinity_peridot and update configurations

--- infinity_peridot.mk
+++ infinity_peridot.mk
@@ -9,12 +9,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)
 
-PRODUCT_NAME := lineage_peridot
+PRODUCT_NAME := infinity_peridot
 PRODUCT_DEVICE := peridot
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -29,13 +29,12 @@ TARGET_DISABLE_EPPE := true
 WITH_GMS := true
 TARGET_OPTIMIZED_DEXOPT := true
 TARGET_HAS_UDFPS := true
-EXTRA_UDFPS_ANIMATIONS := true
-TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_ENABLE_BLUR := true
-SURFACE_FLINGER_BOOST := true
+INFINITY_BUILD_TYPE := UNOFFICIAL
+INFINITY_MAINTAINER := BLU
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="peridot_global-user 16 BP2A.250605.031.A3 OS3.0.4.0.WNPMIXM release-keys" \
