5e71213a81f05b76f75891ffefd1ec909980bee5
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-05-02 14:47:52 +0700

    Update lineage_peridot.mk

--- lineage_peridot.mk
+++ lineage_peridot.mk
@@ -29,13 +29,27 @@ TARGET_DISABLE_EPPE := true
 WITH_GMS := true
 TARGET_OPTIMIZED_DEXOPT := true
 TARGET_HAS_UDFPS := true
-EXTRA_UDFPS_ANIMATIONS := true
 TARGET_SUPPORTED_REFRESH_RATES := 60,90,120
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_ENABLE_BLUR := true
 SURFACE_FLINGER_BOOST := true
+PRODUCT_NO_CAMERA := true
+PERF_GOV_SUPPORTED := true
+PERF_DEFAULT_GOV := schedutil
+TARGET_INCLUDE_AXFX := true
+TARGET_INCLUDES_LOS_PREBUILTS := false
+
+# Camera information (multiple sensors supported)
+AXION_CAMERA_REAR_INFO := 50,8
+AXION_CAMERA_FRONT_INFO := 20
+
+# Maintainer name (underscores become spaces in the UI)
+AXION_MAINTAINER := BLU
+
+# Processor name (underscores become spaces)
+AXION_PROCESSOR := Snapdragon_8s_Gen_3
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BuildDesc="peridot_global-user 16 BP2A.250605.031.A3 OS3.0.4.0.WNPMIXM release-keys" \
